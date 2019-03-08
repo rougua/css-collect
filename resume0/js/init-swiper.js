d4c815a74a0e4a99f524a4102d091cdf8fc81d16
rougua <guakun@aliyun.com>
2019-03-08 11:03:47 +0800

    写完 dom

---
+++ resume0/js/init-swiper.js
@@ -0,0 +1,28 @@
+!function () {
+    const view = e('#mySliders')
+    
+    const controller = {
+        view: null,
+        swiper: null,
+        swiperOptions: {
+            loop: true,
+            pagination: { el: '.swiper-pagination',},
+            navigation: { 
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        },
+        init: function (view) {
+            this.view = view
+            this.initSwiper()
+        },
+        initSwiper: function () {
+            this.swiper = new Swiper(
+                find(this.view, '.swiper-container'),
+                this.swiperOptions
+            )
+        }
+    }
+
+    controller.init(view)
+}()
